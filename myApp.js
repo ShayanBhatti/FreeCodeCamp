f1a6a3cf5ef9f4540741c67423ea511b478ddaa8
ShayanBhatti <shayanbhatti332@gmail.com>
2024-10-14 13:55:29 +0500

    error fixed

--- myApp.js
+++ myApp.js
@@ -18,6 +18,8 @@ var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+
 
  
 

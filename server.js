2ca64f20f0151342f27fd9f319cd03e60bfa24ab
ShayanBhatti <shayanbhatti332@gmail.com>
2024-10-16 06:11:33 +0500

    added bcrypt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 var express = require("express");
 var app = express();
 app.disable("x-powered-by");
-let bcrypt = require("bcrypt");
+var bcrypt = require("bcrypt");
 var fs = require("fs");
 var path = require("path");
 

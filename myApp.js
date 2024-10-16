f6a18d5446f9f230b1274766895a02a177ef5375
ShayanBhatti <shayanbhatti332@gmail.com>
2024-10-16 05:57:48 +0500

    policy control

--- myApp.js
+++ myApp.js
@@ -19,7 +19,7 @@ app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self"], scriptSrc:["'self'","trusted-cdn.com"]}}
+app.use(helmet.contentSecurityPolicy({directives:{defaultSrc:["'self'"], scriptSrc:["'self'","trusted-cdn.com"]}}
 ))
 
  
